3a8ce9a9cabdd8ce8a794f2cd6b12ca61582968c
Erin Kerr <erin.kerr17@gmail.com>
2025-03-09 21:14:05 -0600

    add function to check for a winner

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -4,14 +4,45 @@ let boxes = document.querySelectorAll('.box')
 // start with player 1
 let currentPlayer = 'X'
 
-boxes.forEach((box) => {
+// create a board state to track moves
+let boardState = ['', '', '', '', '', '', '', '', '']
+
+function checkWinner(){
+    // check all winning combinations
+    const winningCombinations = [
+        [0, 1, 2], // Row 1
+        [3, 4, 5], // Row 2
+        [6, 7, 8], // Row 3
+        [0, 3, 6], // Column 1
+        [1, 4, 7], // Column 2
+        [2, 5, 8], // Column 3
+        [0, 4, 8], // Diagonal 1
+        [2, 4, 6]  // Diagonal 2
+    ];
+
+    for (let combo of winningCombinations){
+        const [zero, one, two] = combo;
+        if (boardState[zero] !== '' && 
+            boardState [zero] === boardState[one] &&
+            boardState[one] === boardState[two])
+            alert(`${currentPlayer} wins!`)
+    }
+}
+
+
+
+boxes.forEach((box, index) => {
     box.addEventListener('click', function() {
         // check if box is empty 
-        if (box.innerText = '') {
+        if (box.innerText === '') {
+            boardState[index] = currentPlayer // update board state
+            console.log(boardState[0])
            box.innerText = currentPlayer // set the symbol (X or O)
            checkWinner(); // check for a winner
            box.disabled = true // disable the box
-           currentPlayer = 'O' // switch the player's turn
+           currentPlayer = currentPlayer === 'X' ? 'O' : 'X' // switch the player's turn dynamically
         }
     })
 })
+
+
